6d14308bf331cc7774372ff0c6e9edc8104fec50
Christian <tianqueal@gmail.com>
2024-06-07 16:25:14 +0200

    feat: Update Dockerfile and cloudbuild.yaml for Discord client deployment

--- src/discord-client/config.js
+++ src/discord-client/config.js
@@ -2,6 +2,8 @@ module.exports = {
   PORT: process.env.PORT,
   token: process.env.TOKEN,
   apiUrl: process.env.API_URL,
+  frontendUrl: process.env.FRONTEND_URL,
   clientId: process.env.CLIENT_ID,
+  appName: process.env.APP_NAME,
   guildId: null,
 }
