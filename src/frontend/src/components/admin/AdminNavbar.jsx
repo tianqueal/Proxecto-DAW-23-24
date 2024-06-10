992200a7c24b1f4949310b23b8c36e6220c5e99c
Christian <tianqueal@gmail.com>
2024-06-10 16:25:27 +0200

    feat: Add AdminNavbar component for the admin dashboard

---
+++ src/frontend/src/components/admin/AdminNavbar.jsx
@@ -0,0 +1,116 @@
+import { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import DropdownWindow from '../navigations/DropdownWindow'
+import DropdownIcon from '../navigations/DropdownIcon'
+import ChartBar from '../../assets/heroicons/ChartBar'
+import Users from '../../assets/heroicons/Users'
+import Pencil from '../../assets/heroicons/Pencil'
+import Hashtag from '../../assets/heroicons/Hashtag'
+
+const NavbarItem = ({ to, children }) => {
+  return (
+    <li className="list-none">
+      <Link
+        to={to}
+        className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none dark:text-white dark:hover:bg-gray-700 "
+      >
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+NavbarItem.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+const NavbarGroup = ({ className, onClick }) => {
+  return (
+    <ul className={className} onClick={onClick}>
+      <NavbarItem to="/admin/dashboard">
+        <figure>
+          <ChartBar className="size-5" />
+        </figure>
+        <>Dashboard</>
+      </NavbarItem>
+      <NavbarItem to="/admin/users">
+        <figure>
+          <Users className="size-5" />
+        </figure>
+        <>Usuarios</>
+      </NavbarItem>
+      <NavbarItem to="/admin/notes">
+        <figure>
+          <Pencil className="size-5" />
+        </figure>
+        <>Notas</>
+      </NavbarItem>
+      <NavbarItem to="/admin/topics">
+        <figure>
+          <Hashtag className="size-5" />
+        </figure>
+        <>Temas</>
+      </NavbarItem>
+    </ul>
+  )
+}
+
+NavbarGroup.propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default function AdminNavbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef(null)
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isOpen && ref.current && !ref.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [isOpen, setIsOpen])
+
+  return (
+    <header className="mb-8 mt-4" ref={ref}>
+      <nav className="relative mx-auto rounded-md bg-white px-4 text-gray-800 shadow-md dark:bg-gray-800 dark:text-white sm:px-6 md:max-w-max lg:px-8">
+        <div className="flex h-16 items-center justify-center">
+          <section className="flex items-center">
+            <nav className="hidden md:block">
+              <NavbarGroup
+                className="flex items-baseline space-x-4"
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            </nav>
+          </section>
+          <button
+            onClick={toggleNavbar}
+            className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-600 hover:bg-gray-200 hover:text-white focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:text-white md:hidden"
+          >
+            <DropdownIcon isOpen={isOpen} />
+          </button>
+        </div>
+        <section
+          className={`${isOpen ? 'block' : 'hidden'} w-full rounded-md bg-white px-4 shadow-md dark:bg-gray-800 md:hidden`}
+        >
+          <DropdownWindow customClasses="w-full">
+            <NavbarGroup onClick={() => setIsOpen(!isOpen)} />
+          </DropdownWindow>
+        </section>
+      </nav>
+    </header>
+  )
+}
