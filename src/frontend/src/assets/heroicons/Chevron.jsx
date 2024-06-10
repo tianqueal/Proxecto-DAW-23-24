64644ab5e09e5a291e8034c035c72119ea1f44c2
Christian <tianqueal@gmail.com>
2024-06-10 16:24:36 +0200

    feat: Add and update heroicons

--- src/frontend/src/assets/heroicons/Chevron.jsx
+++ src/frontend/src/assets/heroicons/Chevron.jsx
@@ -1,6 +1,6 @@
 import { PropTypes } from 'prop-types'
 
-export default function ChevronRight({ className, ...props }) {
+export default function Chevron({ className, ...props }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -11,13 +11,13 @@ export default function ChevronRight({ className, ...props }) {
     >
       <path
         fillRule="evenodd"
-        d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+        d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z"
         clipRule="evenodd"
       />
     </svg>
   )
 }
 
-ChevronRight.propTypes = {
+Chevron.propTypes = {
   className: PropTypes.string,
 }
