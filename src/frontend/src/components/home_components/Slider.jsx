738cdf89a079d452219a38f1122ad8e54bf29ef0
Christian <tianqueal@gmail.com>
2024-06-12 22:26:50 +0200

    feat: Update Slider component to pause animation on mouse enter and resume on mouse leave

--- src/frontend/src/components/home_components/Slider.jsx
+++ src/frontend/src/components/home_components/Slider.jsx
@@ -1,14 +1,25 @@
 import { PropTypes } from 'prop-types'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 export default function Slider({ children, rtl = false }) {
+  const [isPaused, setIsPaused] = useState(false)
   const animationClass = rtl ? 'animate-slide-right' : 'animate-slide-left'
+
   return (
-    <div className="logos pause-animation group relative overflow-hidden whitespace-nowrap [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
+    <div
+      className="logos group relative overflow-hidden whitespace-nowrap [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]"
+      onMouseEnter={() => setIsPaused(true)}
+      onMouseLeave={() => setIsPaused(false)}
+    >
       <motion.div
-        className={`group-hover:pause-animation inline-block p-2 ${animationClass}`}
+        className={`inline-block p-2 ${animationClass}`}
         animate={{ x: rtl ? 100 : -100 }}
         transition={{ repeat: Infinity, duration: 10, ease: 'linear' }}
+        style={{
+          x: rtl ? 100 : -100,
+          animationPlayState: isPaused ? 'paused' : 'running',
+        }}
       >
         <div className="flex w-full gap-3">{children}</div>
       </motion.div>
