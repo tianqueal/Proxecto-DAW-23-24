7fc0b90f1e1dbc3813d69d877019a9b8690f000f
Christian <tianqueal@gmail.com>
2024-06-12 21:38:57 +0200

    feat: Mejorar la tabla de temas en la vista de administrador
    Acciones adicionales de la API no han sido implementadas en la futura Release estable, por lo que se comentan en el código y abre la posibilidad de crear una funcionalidad en la API para agregar nuevas traducciones de temas que se puedan agregar en conjunto desde el Frontend

--- src/frontend/src/views/admin/Topics.jsx
+++ src/frontend/src/views/admin/Topics.jsx
@@ -1,35 +1,72 @@
 import { useState } from 'react'
 import { PropTypes } from 'prop-types'
-import Button from '../../components/form/Button'
+import { AnimatePresence, motion } from 'framer-motion'
+// import Button from '../../components/form/Button'
 import Table from '../../components/tables/Table'
 import TableSkeleton from '../../components/skeletons/TableSkeleton'
 import useAdminTopics from '../../hooks/admin/useAdminTopics'
+import { Language } from '../../helpers/constants'
+import AnimatedContent from '../../components/contents/AnimatedContent'
+import ErrorContent from '../../components/contents/ErrorContent'
+import useApi from '../../hooks/useApi'
+import SuccessToastify from '../../components/alerts/SuccessToastify'
+import ErrorToastify from '../../components/alerts/ErrorToastify'
 
-function Header({ onCreate }) {
+function Header(/* { onCreate } */) {
   return (
     <header className="mb-4 flex flex-col items-center justify-between gap-3 md:flex-row">
       <h1 className="text-3xl font-bold">Temas</h1>
-      <Button
+      {/* <Button
         type="button"
         onClick={onCreate}
         className="bg-blue-500 text-white ring-blue-600 hover:bg-blue-600 focus:bg-blue-600 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:bg-blue-900"
         text="Crear un nuevo tema"
-      />
+      /> */}
     </header>
   )
 }
 
 Header.propTypes = {
-  onCreate: PropTypes.func.isRequired,
+  onCreate: PropTypes.func,
 }
 
 export default function Topics() {
+  const { deleteAdminTopic } = useApi()
   const [page, setPage] = useState(1)
-  const { topics, isError, isLoading, isValidating, totalPages, links } =
-    useAdminTopics(page)
+  const [isActionLoading, setIsActionLoading] = useState({})
+  const {
+    topics,
+    isError,
+    isLoading,
+    isValidating,
+    totalPages,
+    links,
+    mutate,
+  } = useAdminTopics(page)
 
-  const handleCreateUser = () => {
-    console.log('Crear tema')
+  const onSuccessfulAction = async ({ message }) => {
+    await mutate()
+    SuccessToastify({ message })
+    setIsActionLoading({})
+  }
+
+  const onErroredAction = () => {
+    ErrorToastify({ message: 'Ha ocurrido un error', autoClose: true })
+  }
+
+  /* const handleCreateTopic = () => {
+      console.log('Crear tema')
+  } */
+
+  const handleDeleteTopic = async (id) => {
+    if (!window.confirm('¿Estás seguro de eliminar este tema?')) return
+    setIsActionLoading({ id })
+    await deleteAdminTopic({
+      id,
+      setIsLoading: setIsActionLoading,
+      onSuccess: onSuccessfulAction,
+      onError: onErroredAction,
+    })
   }
 
   const columns = [
@@ -39,22 +76,26 @@ export default function Topics() {
     },
     {
       key: 'name',
-      label: 'Nombre',
+      label: `Nombre (${Language.CURRENT})`,
+    },
+    {
+      key: 'originalName',
+      label: 'Nombre original',
     },
   ]
 
   const actions = [
-    {
+    /* {
       label: 'Edit',
       onClick: (id) => console.log('Edit', id),
       textColor: 'text-yellow-600',
       hoverTextColor: 'hover:text-yellow-800',
       darkTextColor: 'dark:text-yellow-400',
       darkHoverTextColor: 'dark:hover:text-yellow-200',
-    },
+    }, */
     {
-      label: 'Delete',
-      onClick: (id) => console.log('Delete', id),
+      label: 'Eliminar',
+      onClick: handleDeleteTopic,
       textColor: 'text-red-600',
       hoverTextColor: 'hover:text-red-800',
       darkTextColor: 'dark:text-red-400',
@@ -67,23 +108,42 @@ export default function Topics() {
   }
 
   return (
-    <>
-      <Header onCreate={handleCreateUser} />
-      {(isLoading || isError) && <TableSkeleton className="w-full" />}
-      {isError && <p className="text-xl font-medium">Ha ocurrido un error</p>}
-      {!isLoading && !isError && !isValidating && topics?.length === 0 && (
-        <p className="text-xl font-medium">¡Sin temas!</p>
-      )}
-      {!isLoading && topics && topics.length > 0 && (
-        <Table
-          data={topics}
-          columns={columns}
-          actions={actions}
-          totalPages={totalPages}
-          links={links}
-          onPageChange={handlePageChange}
-        />
-      )}
-    </>
+    <AnimatePresence>
+      <motion.section
+        key="section"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <Header /* onCreate={handleCreateTopic} */ />
+
+        {(isLoading || isError) && (
+          <AnimatedContent keyProp="loader">
+            <TableSkeleton className="w-full scale-110" />
+          </AnimatedContent>
+        )}
+
+        {isError && <ErrorContent />}
+
+        {!isLoading && !isError && !isValidating && topics?.length === 0 && (
+          <ErrorContent keyProp="no-topics" description="¡Sin temas!" />
+        )}
+
+        {!isLoading && topics && topics.length > 0 && (
+          <AnimatedContent keyProp="table">
+            <Table
+              data={topics}
+              columns={columns}
+              actions={actions}
+              isActionLoading={isActionLoading}
+              totalPages={totalPages}
+              links={links}
+              onPageChange={handlePageChange}
+            />
+          </AnimatedContent>
+        )}
+      </motion.section>
+    </AnimatePresence>
   )
 }
