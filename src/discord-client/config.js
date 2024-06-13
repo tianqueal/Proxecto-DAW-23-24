019ba1aa96962095b4306b20e414602e8e4e798c
Christian <tianqueal@gmail.com>
2024-06-13 04:54:39 +0200

    chore: Update frontendUrl handling in config.js

--- src/discord-client/config.js
+++ src/discord-client/config.js
@@ -2,7 +2,8 @@ module.exports = {
   PORT: process.env.PORT,
   token: process.env.TOKEN,
   apiUrl: process.env.API_URL,
-  frontendUrl: process.env.FRONTEND_URL,
+  frontendUrl:
+    process.env.FRONTEND_URL.split("=")[1] ?? process.env.FRONTEND_URL,
   clientId: process.env.CLIENT_ID,
   appName: process.env.APP_NAME,
   guildId: null,
