3304785257c59654f1f438ff752eaea917f61dc9
Christian <tianqueal@gmail.com>
2024-06-14 18:05:20 +0200

    feat: Update CSS class name in InputField component

--- src/frontend/src/components/form/InputField.jsx
+++ src/frontend/src/components/form/InputField.jsx
@@ -69,7 +69,7 @@ function InputElement({
             ref={inputRef}
             onChange={onChange}
             autoComplete={autoComplete}
-            className={`mt-1 block w-full rounded-md px-3 py-2 shadow-sm transition-all dark:bg-gray-900 dark:text-gray-100 sm:text-sm ${
+            className={`block w-full rounded-md px-3 py-2 shadow-sm transition-all dark:bg-gray-900 dark:text-gray-100 sm:text-sm ${
               errorText
                 ? 'border border-red-500 focus:border-red-500 focus:ring-red-500 dark:border-red-400 dark:focus:border-red-400 dark:focus:ring-red-400'
                 : 'border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500  dark:border-gray-600 dark:focus:border-indigo-500 dark:focus:ring-indigo-500'
