18e3e8ee95670f290a44a9e209177768a31d87f3
Christian <tianqueal@gmail.com>
2024-06-16 15:00:40 +0200

    feat: Update dependencies and refactor isValidPath function for better security and readability

--- src/discord-client/index.js
+++ src/discord-client/index.js
@@ -7,6 +7,8 @@ const app = express()
 const port = config.PORT || 80
 const { setGlobalDispatcher, Agent, Pool } = require("undici")
 const rateLimit = require("express-rate-limit")
+const sanitizeFilename = require("sanitize-filename")
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -20,8 +22,9 @@ setGlobalDispatcher(
 
 function isValidPath(requestedPath) {
   const publicDir = path.join(__dirname, "public")
-  const fullPath = path.join(publicDir, requestedPath)
-  const normalizedPath = path.normalize(fullPath)
+  const sanitizedPath = sanitizeFilename(requestedPath)
+  const fullPath = path.join(publicDir, sanitizedPath)
+  const normalizedPath = path.resolve(fullPath)
 
   return (
     normalizedPath.startsWith(publicDir + path.sep) &&
@@ -29,10 +32,28 @@ function isValidPath(requestedPath) {
   )
 }
 
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", function (_, res) {
+  const indexPath = path.join(__dirname, "public", "index.html")
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      res.status(404).send("Not found")
+    }
+  })
+})
+
 app.get("/:path", function (req, res) {
-  let requestedPath = req.params.path
+  const requestedPath = req.params.path
   if (isValidPath(requestedPath)) {
-    res.sendFile(path.join(__dirname, "public", requestedPath))
+    res.sendFile(
+      path.join(__dirname, "public", sanitizeFilename(requestedPath)),
+      (err) => {
+        if (err) {
+          res.status(404).send("Not found")
+        }
+      }
+    )
   } else {
     res.status(404).send("Not found")
   }
